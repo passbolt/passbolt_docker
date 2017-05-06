5157f6e22ef64c60e31d03cdd41801a58ab367f3
Albert Casals <skarbat@gmail.com>
2017-05-06 15:53:53 +0000

    do not start multiple times, fixed tcp port mappings

--- raspberry/startup.sh
+++ raspberry/startup.sh
@@ -11,28 +11,33 @@ source pivars
 
 function start_mysql() {
 
-    echo "Starting $image_mysql..."
-    docker run -d -e MYSQL_ROOT_PASSWORD="" \
-       -e MYSQL_ALLOW_EMPTY_PASSWORD=1 \
-       -e MYSQL_RANDOM_ROOT_PASSWORD=1 \
-       -e MYSQL_DATABASE=passbolt \
-       -e MYSQL_USER=passbolt \
-       -e MYSQL_PASSWORD=P4ssb0lt \
-       $image_mysql
+    id_mysql=`docker ps -aq --filter ancestor=$image_mysql --filter status=running`
+    if [ "$id_mysql" != "" ]; then
+	echo "$image_mysql is already running $id_mysql"
+    else
+	echo "Starting $image_mysql..."
+	docker run -d -e MYSQL_ROOT_PASSWORD="" \
+	       -e MYSQL_ALLOW_EMPTY_PASSWORD=1 \
+	       -e MYSQL_RANDOM_ROOT_PASSWORD=1 \
+	       -e MYSQL_DATABASE=passbolt \
+	       -e MYSQL_USER=passbolt \
+	       -e MYSQL_PASSWORD=P4ssb0lt \
+	       $image_mysql
 
-    sleep 5
+	sleep 5
+    fi
 }
 
 function start_passbolt() {
 
     id_passbolt=`docker ps -aq --filter ancestor=$container_passbolt --filter status=running`
     if [ "$id_passbolt" != "" ]; then
-	echo "Passbolt container is already running $id_passbolt"
+	echo "$container_passbolt is already running $id_passbolt"
     else
 	id_mysql=`docker ps -aq --filter ancestor=$image_mysql --filter status=running`
 	ipaddress=`docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $id_mysql`
 	echo "Starting $container_passbolt container binding IP=$ipaddress"
-	docker run -d -e db_host=$ipaddress $container_passbolt
+	docker run -d -p 80:80 -p 443:443 -e db_host=$ipaddress $container_passbolt
     fi
 }
 
