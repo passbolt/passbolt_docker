1d6baa8e0ad46e10872fd1e3654e28c2a1030bfb
Albert Casals <skarbat@gmail.com>
2017-05-06 15:23:22 +0000

    complete the startup script

--- raspberry/startup.sh
+++ raspberry/startup.sh
@@ -2,37 +2,41 @@
 #
 # Script to start the containers
 #
+# WARNING:The session used to run this script must stay alive all the time.
+#
 
 set -e
 
-source rpi_variables
+source pivars
 
 function start_mysql() {
 
-    docker run -e MYSQL_ROOT_PASSWORD="" \
+    echo "Starting $image_mysql..."
+    docker run -d -e MYSQL_ROOT_PASSWORD="" \
        -e MYSQL_ALLOW_EMPTY_PASSWORD=1 \
        -e MYSQL_RANDOM_ROOT_PASSWORD=1 \
        -e MYSQL_DATABASE=passbolt \
        -e MYSQL_USER=passbolt \
        -e MYSQL_PASSWORD=P4ssb0lt \
        $image_mysql
+
+    sleep 5
 }
 
 function start_passbolt() {
 
-    image_passbolt=`docker images $container_passbolt"`
-    if [ "$image_passbolt" != "" ]; then
-	echo "Passbolt container already built: $id_passbolt"
+    id_passbolt=`docker ps -aq --filter ancestor=$container_passbolt --filter status=running`
+    if [ "$id_passbolt" != "" ]; then
+	echo "Passbolt container is already running $id_passbolt"
     else
 	id_mysql=`docker ps -aq --filter ancestor=$image_mysql --filter status=running`
-	ip=`docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $id_mysql`
-	echo "id=$id_mysql ip=$ip"
-	#docker run -e db_host=??? $container_passbolt
+	ipaddress=`docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $id_mysql`
+	echo "Starting $container_passbolt container binding IP=$ipaddress"
+	docker run -d -e db_host=$ipaddress $container_passbolt
     fi
 }
 
 
-# Start the passbolt container
-# TODO: move below steps to an external script
+# Start mysql and passbolt containers
 start_mysql
 start_passbolt
